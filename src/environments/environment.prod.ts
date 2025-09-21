260c5867a392a4ef59350b941d36565830eba85e
niaraaaa <niaramadani@mail.ugm.ac.id>
2025-09-21 21:29:49 +0700

    Initial commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBmHhjbWNcu0d3qO_dX7LjVN2V_B-rRS2U",
+    authDomain: "pgpbl-ionic-4bd41.firebaseapp.com",
+    projectId: "pgpbl-ionic-4bd41",
+    storageBucket: "pgpbl-ionic-4bd41.firebasestorage.app",
+    messagingSenderId: "367422138402",
+    appId: "1:367422138402:web:33a736677ae578b512027b"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyBmHhjbWNcu0d3qO_dX7LjVN2V_B-rRS2U",
+    authDomain: "pgpbl-ionic-4bd41.firebaseapp.com",
+    projectId: "pgpbl-ionic-4bd41",
+    storageBucket: "pgpbl-ionic-4bd41.firebasestorage.app",
+    messagingSenderId: "367422138402",
+    appId: "1:367422138402:web:33a736677ae578b512027b"
+  }
 };
 
 /*
